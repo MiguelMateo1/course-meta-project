9d46b634d3318c6b27093e9ee415e55085898fe9
Miguel Mateo <mmateo95@hotmail.com>
2023-04-11 14:24:58 -0400

    ajusted return content and adde Route components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import Header from './components/Header'
 import Nav from './components/Nav'
-import Main from './components/Main'
 import Footer from './components/Footer'
+import {Route,Routes} from "react-router-dom"
 import './App.css';
 
 function App() {
   return (
     <>
       <Header />
+      <Routes> 
+        <Route path="/" exact element={<Main />}></Route>
+        <Route path="/booking" element={<BookingPage />}></Route> 
+        <Route path="/confirmed" element={<ConfirmedBooking />}></Route>
+      </Routes>
       <Nav />
-      <Main />
       <Footer />
    </>
   );
