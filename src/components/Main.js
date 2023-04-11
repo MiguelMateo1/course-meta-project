bbc74bbb1eb775c6a276c8f7f9704cafadbeb732
Miguel Mateo <mmateo95@hotmail.com>
2023-04-11 14:26:50 -0400

    added componets layouts on the return

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,15 @@
+import Hero from "./components/Hero"
+import Menu from "./components/Menu"
+import Testimonials from "./components/Testimonials"
+import Description from "./components/Description"
+
 function Main() {
     return (
       <main>
-
+         <Hero />
+         <Menu />
+         <Testimonials />
+         <Description />
       </main>
     );
   }
