b0f92bd5d49c88c1de13eb3e309c8585a72ceede
Miguel Mateo <mmateo95@hotmail.com>
2023-04-11 19:38:09 -0400

    main pages component created

---
+++ src/components/ConfirmedBooking.js
@@ -0,0 +1,11 @@
+
+function ConfirmedBooking() {
+    return (
+      <main className="confirmed-main">
+       <p className="title confirmed-title">Booking Confirmed!</p>
+       <p className="subtitle confirmed-subtitle">We are excited for your visit.</p>
+      </main>
+    )
+  };
+
+  export default ConfirmedBooking;
